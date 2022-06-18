3f9878a16e001f4a88632c57725eb1b342078d3b
webdotG <webdotg@gmail.com>
2022-06-18 12:44:43 +0300

    change class visual hidden js

--- js/feedback-form.js
+++ js/feedback-form.js
@@ -1,13 +1,19 @@
 let formOpen = document.querySelector(".cart-all-in-all__order-btn");
 let formClose = document.querySelector(".feedback-form__btn-close");
 let feedbackForm = document.querySelector('.feedback-form__wrapper');
+let productHint = document.querySelector('.product-remove__hint');
+
 
 formOpen.addEventListener('click', function (evt){
     evt.preventDefault();
-    feedbackForm.classList.remove('visual-hidden');
+    feedbackForm.classList.remove('display-none');
 })
 
 formClose.addEventListener('click', function (evt){
     evt.preventDefault();
-    feedbackForm.classList.add('visual-hidden');
+    feedbackForm.classList.add('display-none');
 })
+
+//productHint.addEventListener('click',( evt) {
+  //  evt.preventDefault()
+//} )

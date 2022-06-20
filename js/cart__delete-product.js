d649438b822b34f9e19158392b9f9d26f1a03151
webdotG <webdotg@gmail.com>
2022-06-20 13:08:20 +0300

    cart dell product js add

---
+++ js/cart__delete-product.js
@@ -0,0 +1,7 @@
+let productDel = document.querySelectorAll('.product-remove__btn');
+
+for (let i = 0; i < productDel.length; i++ ){
+productDel[i].addEventListener('click', function (){
+    productDel[i].parentElement.parentElement.classList.add('display-none');
+})
+}

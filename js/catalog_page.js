3690f42c0742aeb08efd3e36945eeacd86193f93
webdotG <webdotg@gmail.com>
2022-06-24 17:25:45 +0300

    custom checkbox add working version

--- js/catalog_page.js
+++ js/catalog_page.js
@@ -1,11 +1,19 @@
 let sortingPriceBtn = document.querySelector(".sorting-main__button--price");
 let sortingPriceForm = document.querySelector(".sorting-main__price");
-
+let sortingBrandBtn = document.querySelector(".sorting-main__button--brand");
+let sortingBrandForm = document.querySelector(".sorting-main__brand");
 
 sortingPriceBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
     sortingPriceForm.classList.remove('display-none');
 })
+sortingBrandBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    sortingBrandForm.classList.remove('display-none');
+})
+
+
+
 //------------------------------------прерываю событие ------------------------------------------
 //------------------------------------работает но в данном случае отоброжается ек так как нам надо-----------------------
 //sortingPriceBtn.addEventListener(`click`, (e) => {

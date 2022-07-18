4fc18a9e9c8e379332ec8fb2507a41fad93c1829
webdotG <webdotg@gmail.com>
2022-07-18 15:44:53 +0300

    mobile and desktop version cart page done

--- js/feedback-form.js
+++ js/feedback-form.js
@@ -1,17 +1,18 @@
 let formOpen = document.querySelector(".cart-all-in-all__order-btn");
 let formClose = document.querySelector(".feedback-form__btn-close");
 let feedbackForm = document.querySelector('.feedback-form__wrapper');
-let productHint = document.querySelector('.product-remove__hint');
 
-
-formOpen.addEventListener('click', function (evt){
-    evt.preventDefault();
+console.log(formOpen);
+formOpen.addEventListener('click', function (evt) {
+    console.log('TEST');
     feedbackForm.classList.remove('display-none');
+    evt.preventDefault()
 })
 
 formClose.addEventListener('click', function (evt){
-    evt.preventDefault();
+    console.log('TEST');
     feedbackForm.classList.add('display-none');
+    evt.preventDefault()
 })
 
 

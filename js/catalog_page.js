580ee20e0571a9f9489958835164cc29cfc42d48
webdotG <webdotg@gmail.com>
2022-07-19 16:58:57 +0300

    catalog btn fix js

--- js/catalog_page.js
+++ js/catalog_page.js
@@ -2,6 +2,9 @@ let sortingPriceBtn = document.querySelector(".sorting-main__button--price");
 let sortingPriceForm = document.querySelector(".sorting-main__price");
 let sortingBrandBtn = document.querySelector(".sorting-main__button--brand");
 let sortingBrandForm = document.querySelector(".sorting-main__brand");
+let sortingInOrderBtn = document.querySelector(".sorting-main__button--expensive");
+let sortingInOrderForm = document.querySelector('.catalog-page__sorting-sub-menu-list')
+
 
 sortingPriceBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
@@ -10,6 +13,11 @@ sortingPriceBtn.addEventListener('click', function (evt) {
 sortingBrandBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
     sortingBrandForm.classList.toggle('display-none');
+})
+
+sortingInOrderBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    sortingInOrderForm.classList.toggle('display-none');
 })
 
 
